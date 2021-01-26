dcb9d2365073330bcfca7ad7b80bb279c2622a12
Marek0200 <76998761+Marek0200@users.noreply.github.com>
2021-01-26 13:37:01 +0100

    ADD1

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ $('.button1').click(() => {
     $(".button1").text('Yeah, you clicked me');
   });
 
-  $(".button3").click(() => {
-      $("button").css("background-color", "red")
-  })
+  
+//$(".button3").click(() => {
+   // $("button").css("background-color", "red")
+//});
+
+
+$(".button3").click(() => {
+  $('button').css('background-color',"[inputValue]");
+});
+
